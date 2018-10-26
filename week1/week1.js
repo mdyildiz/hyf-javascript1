92060a91f6b56a04a8aee381b1ab03b32d218e15
Deniz <43441269+mdyildiz@users.noreply.github.com>
2018-10-26 12:43:50 +0200

    Add files via upload

---
+++ week1/week1.js
@@ -0,0 +1,147 @@
+Exercise 1
+
+let helloWorld= "Hallo, Wereld!"\n , "Hello, World!"\n  , "Merhaba, Dunya!"; 
+console.log(helloWorld);
+
+Exercise 2
+
+console.log("I'm awesome");
+
+Exercise 3
+
+let myVariable = "x";
+console.log("the value of myVariable will be undefined);
+x=10;
+console.log("the value of myVariable will be 10");
+console.log(myVariable);
+
+Exercise 4
+
+let mySonName = "y";
+y="Omer";
+console.log("the value of my string will be Omer");
+console.log(mySonName);
+y="Faruk";
+console.log("the value of my string will be Faruk");
+console.log(mySonName);
+
+Exercise 5
+
+let assignedNumber = "z";
+z=7.25;
+console.log(assignedNumber);
+let roundNumber="a"
+a=Math.round (assignedNumber);
+console.log(a);
+let highestValue=Math.max(assignedNumber,roundNumber);
+console.log(highestValue);
+
+Exercise 6
+
+let myFavoriteAnimals=[" "];
+console.log("the value of my array is [" "]");
+console.log(myFavoriteAnimals);
+let myFavoriteAnimals=["Cat" , "Sheep" , "Rabbit"];
+console.log(myFavoriteAnimals);
+myFavoriteAnimals.push("baby pig");
+console.log(myFavoriteAnimals);
+
+Exercise 7
+
+let myString = "this is a test";
+console.log(myString);
+console.log(myString.length);
+
+Exercise 8
+
+let a="10 > 9"
+console.log('The value of my variable a is: ' + a);
+let b="";
+console.log('The value of my variable b is: ' + b);
+let c=5;
+console.log('The value of my variable c is: ' + c);
+let d="John";
+console.log('The value of my variable d is: ' + d);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+if (a===b) {
+    console.log('SAME TYPE');
+}
+if (c===d) {
+    console.log('SAME TYPE');
+}
+if (a===c) {
+    console.log('SAME TYPE');
+}
+if (a===d) {
+    console.log('SAME TYPE');
+}
+
+Exercise 9
+
+let x=7;
+console.log(x = x % 3);
+console.log("The new value of x would be : 1");
+
+let x = 10;
+let y = 4;
+let z = x % y;
+console.log (z);
+console.log ("The value of z would be : 2");
+
+let x = 20;
+let y = 5;
+let z = x % y;
+console.log (z);
+console.log ("The value of z would be : 0");
+
+let x = 50;
+let y = 8;
+let z = x % y;
+console.log (z);
+console.log ("The value of z would be : 2");
+
+Exercise 10
+
+let elements = ["Hello", 7, true];
+console.log(elements);
+
+let x = 6/0;
+let y = 10/0;
+if ( x === y )
+    console.log ( "Equal type" );
+else
+{
+console.log ( "Different type");
+}
+
+let a = 10/2;
+let b = 8/4;
+if ( a === b )
+    console.log ( "Equal type" );
+else
+{
+console.log ( "Different type");
+}
+
+let x = 12/4 ;
+let y = "Text";
+if ( x === y )
+    console.log ( "Equal type" );
+else
+{
+console.log ( "Different type");
+}
+
+
+
+
+
+
+
+
+
+
+
