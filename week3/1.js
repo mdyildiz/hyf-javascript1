38ecbebf928931cf88185c6ca727a107839ff6de
Deniz <43441269+mdyildiz@users.noreply.github.com>
2018-11-06 23:57:56 +0100

    Delete 1.js

--- week3/1.js
+++
@@ -1,4 +0,0 @@
-let myString = "hello,this,is,a,difficult,to,read,sentence";
-console.log(myString.length);
-myString = "hello " + "this " + "is " + "a " + "difficult " + "to " + "read " + "sentence";
-console.log(myString);
